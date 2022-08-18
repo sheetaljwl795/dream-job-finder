11e155b3a5ca622b77eec9062562207179988594
slendyG <josuegarza0608@gmail.com>
2022-08-18 13:51:49 -0700

    Added weather api call based on user zip.

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ var sortOrder = 0;
 var startRecord = 0;
 var pageSize = 25;
 var daysEl = document.querySelector('#days');
-var jobsContainerEl= document.querySelector('#jobs-container');
+var jobsContainerEl = document.querySelector('#jobs-container');
 var jobSearchTerm = document.querySelector('job-search-term');
 var userFormEl = document.querySelector('#user-form');
 var savedJobs = [];
@@ -15,84 +15,86 @@ var savedJobs = [];
 var weatherZip = document.querySelector('#zip');
 var apiKey = "d91f911bcf2c0f925fb6535547a5ddc9"
 var weatherApiUrl = "https://api.openweathermap.org";
+var weatherSubmit = document.querySelector(".weather-form")
 
+var formSubmitHandler = function (event) {
+  event.preventDefault();
 
-var formSubmitHandler = function(event) {
-    event.preventDefault();
+  var keyword = keywordEl.value.trim();
+  var location = locationEl.value.trim();
+  var radius = radiusEl.value.trim();
+  var days = daysEl.value.trim();
 
-    var keyword = keywordEl.value.trim();
-    var location = locationEl.value.trim();
-    var radius = radiusEl.value.trim();
-    var days = daysEl.value.trim();
-
-    getJobs(keyword,location,radius,days);
+  getJobs(keyword, location, radius, days);
+  fetchWeather();
 };
 
 
-var getJobs = function(val1, val2, val3, val4) {
-    var apiUrl = 'https://api.careeronestop.org/v1/jobsearch/cgdmCgyPmLm9gZv/' + val1 + '/' + val2 + '/' + val3 + '/0/0/0/25/' + val4 + '?source=NLx&showFilters=false';
-    console.log(apiUrl);
-    
-    fetch(apiUrl, {
-        headers: {Authorization: "Bearer fp63rEjnLUMlR+EolzxiYFhWBRxeLUH6GeNtjPUeRBxtpmZl0E76E9iGBGGVeVOpYbTh/U2kf1ao8pD4+0XStA=="}})
+var getJobs = function (val1, val2, val3, val4) {
+  var apiUrl = 'https://api.careeronestop.org/v1/jobsearch/cgdmCgyPmLm9gZv/' + val1 + '/' + val2 + '/' + val3 + '/0/0/0/25/' + val4 + '?source=NLx&showFilters=false';
+  console.log(apiUrl);
+
+  fetch(apiUrl, {
+    headers: { Authorization: "Bearer fp63rEjnLUMlR+EolzxiYFhWBRxeLUH6GeNtjPUeRBxtpmZl0E76E9iGBGGVeVOpYbTh/U2kf1ao8pD4+0XStA==" }
+  })
 
     .then(function (response) {
-        if (response.ok) {
+      if (response.ok) {
         console.log(response);
         response.json().then(function (data) {
-                console.log(data);    
-                console.log(data.Jobs);
-                console.log(keywordEl.value.trim());
-                displayJobs(data.Jobs, keywordEl.value.trim());
+          console.log(data);
+          console.log(data.Jobs);
+          console.log(keywordEl.value.trim());
+          displayJobs(data.Jobs, keywordEl.value.trim());
         });
-        } else {
+      } else {
         alert('Error: ' + response.statusText);
-        }
+      }
     })
-    
+
     .catch(function (error) {
       alert('Unable to connect to CareerOneStop');
     });
 
 }
 
-var displayJobs = function(jobcount, jobsearchKeyword) {
-    if (jobcount.length === 0) {
-        jobsContainerEl.textContent = 'No jobs found.';
-        return;
-      }
-    
-      // jobSearchTerm.textContent = jobsearchKeyword;
-    
-      for (var i = 0; i < jobcount.length; i++) {
-        var jobName = jobcount[i].Company + '/' + jobcount[i].JobTitle + '/' + jobcount[i].Location  +  '/' + jobcount[i].AccquisitionDate ;
+var displayJobs = function (jobcount, jobsearchKeyword) {
+  if (jobcount.length === 0) {
+    jobsContainerEl.textContent = 'No jobs found.';
+    return;
+  }
 
-        // click event around here for weather api call
-    
-        var jobEl = document.createElement('a');
-        jobEl.classList = 'list-item flex-row justify-space-between align-center';
-        jobEl.setAttribute('href', jobcount[i].URL);
-    
-        var titleEl = document.createElement('span');
-        titleEl.textContent = jobName;
-    
-        jobEl.appendChild(titleEl); 
+  // jobSearchTerm.textContent = jobsearchKeyword;
+
+  for (var i = 0; i < jobcount.length; i++) {
+    var jobName = jobcount[i].Company + '/' + jobcount[i].JobTitle + '/' + jobcount[i].Location + '/' + jobcount[i].AccquisitionDate;
+
+    // click event around here for weather api call
+
+    var jobEl = document.createElement('a');
+    jobEl.classList = 'list-item flex-row justify-space-between align-center';
+    jobEl.setAttribute('href', jobcount[i].URL);
+
+    var titleEl = document.createElement('span');
+    titleEl.textContent = jobName;
+
+    jobEl.appendChild(titleEl);
 
 
-        var saveEl = document.createElement('BUTTON');
-        saveEl.classList = 'flex-row align-center';
-        saveEl.innerText = "Save";
-        saveEl.setAttribute('button', "onclick=()");
+    var saveEl = document.createElement('BUTTON');
+    saveEl.classList = 'flex-row align-center';
+    saveEl.innerText = "Save";
+    saveEl.setAttribute('button', "onclick=()");
 
-          // localStorage.setItem("savedJobs")
-          // savedJobs.push(jobName) }");
-      
-        jobEl.appendChild(saveEl);
+    // localStorage.setItem("savedJobs")
+    // savedJobs.push(jobName) }");
 
-        jobsContainerEl.appendChild(jobEl);
+    jobEl.appendChild(saveEl);
 
-      }
-    };
+    jobsContainerEl.appendChild(jobEl);
+
+  }
+};
 
 
 
@@ -101,31 +103,40 @@ userFormEl.addEventListener('submit', formSubmitHandler);
 
 // weather api call
 // based off of zip
-var getWeather = function fetchWeather(location) {
-    var { lat } = location;
-    var { lon } = location;
-    var cityName = location.name;
-    var apiUrl = `${weatherApiUrl}/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly&appid=${apiKey}`;
+//function renderItems(city, data) {
+//   renderCurrentWeather(city, data.current, data.timezone);
+// }
 
-  fetch(weatherApiUrl, {
-      headers: {Authorization: "d91f911bcf2c0f925fb6535547a5ddc9"}})
+function fetchWeather(location) {
+  var zipCode = locationEl.value
+  var apiUrl = `${weatherApiUrl}/data/2.5/forecast?zip=${zipCode},us&appid=${apiKey}`;
 
-  .then(function (response) {
+  fetch(apiUrl)
+
+    .then(function (response) {
       if (response.ok) {
-      console.log(response);
-      response.json().then(function (data) {
-              console.log(data);    
-              console.log(data.Jobs);
-              console.log(weatherZip.value.trim());
-              displayWeather(data.Jobs, weatherZip.value.trim());
-      });
+        console.log(response);
+        response.json()
+        .then(function (data) {
+          console.log(data);
+          console.log(data.city);
+          console.log(weatherZip.value.trim());
+          // renderItems(data.city, weatherZip.value.trim());
+        });
       } else {
-      alert('Error: ' + response.statusText);
+        alert('Error: ' + response.statusText);
       }
-  })
+    })
 }
-  //display jobs function
 
-  var displayWeather = d
-  
+// weather submit button
+var weatherSubmitBtn = function (event) {
+  event.preventDefault();
+}
+weatherSubmit.addEventListener('click', weatherSubmitBtn);
+
+//display jobs function
+
+//var displayWeather = d
+
 // init() function
